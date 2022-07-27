f3006054650c8e79331e8bb65bfa63b20a49b394
B, Prashanth Natraj <Prashanth.Natraj.B@intel.com>
2022-07-27 22:52:27 +0530

    pytest for curated app
    Signed-off-by: B, Prashanth Natraj <Prashanth.Natraj.B@intel.com>

--- src/libs/curated_app_libs.py
+++ src/libs/curated_app_libs.py
@@ -1,15 +1,60 @@
 import inspect
+import subprocess
+import sys
 import yaml
+import os
+
 
 def read_config_yaml(config_file_path, test_name):
     yaml_file = open(config_file_path, "r")
     parsed_yaml_file = yaml.safe_load(yaml_file)
-    test_config = parsed_yaml_file["Default"]
+    test_config = parsed_yaml_file["default_input_args"]
 
-    if parsed_yaml_file.get(test_name):
-        test_items = parsed_yaml_file[test_name]
+    if parsed_yaml_file.get(test_name).get("input_args"):
+        test_items = parsed_yaml_file[test_name]["input_args"]
         test_config.update(test_items)
     return test_config
+
+
+def read_value_config_yaml(config_file_path, test_name, key):
+    yaml_file = open(config_file_path, "r")
+    parsed_yaml_file = yaml.safe_load(yaml_file)
+
+    if parsed_yaml_file.get(test_name).get(key):
+        return parsed_yaml_file.get(test_name).get(key)
+
+
+def sort_dict(test_config_dict):
+    # make sure strings in the order list matches with yaml keys
+    # curated app expects inputs in the following order
+    input_ord_list = ['signing_key_path', 'attestation', 'ca_cert_path', 'runtime_variables',
+                      'runtime_variable_list', 'encrypted_files', 'encrypted_files_path']
+    # sort dictionary based on input order list
+    return {key: test_config_dict[key] for key in input_ord_list if key in test_config_dict.keys()}
+
+
+def get_inputs_from_dict(test_config_dict):
+    input_str = ''
+    for key, value in test_config_dict.items():
+        if value:
+            input_str += str(value).strip() + "\n"
+        else:
+            input_str += "\n"
+    return input_str
+
+
+def create_input_file(input_str):
+    with open(os.getenv('CURATED_APPS_PATH', "") + "input.txt", mode="w") as f:
+        f.write(input_str)
+        f.close()
+
+
+def run_curated_app(base_os):
+    os.chdir(os.getenv('CURATED_APPS_PATH', ""))
+    process = subprocess.Popen(['python3 curation_app.py ' + base_os + ' < input.txt'], stdout=sys.stdout,
+                               stderr=sys.stderr, shell=True)
+    process.communicate()
+    return process.returncode
 
 
 def run_test(test_instance, test_yaml_file):
@@ -17,5 +62,11 @@ def run_test(test_instance, test_yaml_file):
     print(f"\n********** Executing {test_name} **********\n")
     test_config_dict = read_config_yaml(test_yaml_file, test_name)
     print(test_config_dict)
-
-    return 1
+    docker_base_os = read_value_config_yaml(test_yaml_file, test_name, 'docker_image')
+    print(docker_base_os)
+    sorted_dict = sort_dict(test_config_dict)
+    print(sorted_dict)
+    input_str = get_inputs_from_dict(sorted_dict)
+    print(input_str)
+    create_input_file(input_str)
+    return run_curated_app(docker_base_os)
---
+++ test/test_apps_without_attestation.py
@@ -0,0 +1,21 @@
+import os
+import pytest
+import src.libs.curated_app_libs
+
+yaml_file_name = "tests_without_attestation.yaml"
+tests_yaml_path = os.path.join(os.getcwd(), 'data', yaml_file_name)
+
+
+class TestClass:
+
+    def test_redis(self):
+        test_result = src.libs.curated_app_libs.run_test(self, tests_yaml_path)
+        assert test_result == 0
+
+    def test_redis_with_sign_key(self):
+        test_result = src.libs.curated_app_libs.run_test(self, tests_yaml_path)
+        assert test_result == 0
+
+    def test_redis_with_runtime_variables(self):
+        test_result = src.libs.curated_app_libs.run_test(self, tests_yaml_path)
+        assert test_result == 0

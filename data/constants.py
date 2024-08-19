65cfc0ccbe7acc33db60934dc116dd677eafa32b
sreeharikax <sreex.harika.kalakatta@intel.com>
2024-08-19 14:10:56 +0530

    Updated sklearn version

--- data/constants.py
+++ data/constants.py
@@ -83,7 +83,7 @@ OVMS_INIT_DB_CMD            = f"curl --create-dirs https://storage.openvinotoolk
                          -o {OVMS_TESTDB_PATH}/1/face-detection-retail-0004.xml -o {OVMS_TESTDB_PATH}/1/face-detection-retail-0004.bin"
 OVMS                        = "openvino-model-server"
 BASELINE_APP_VERSION        = {"redis": "7.0.11", "mysql": "8.0.36-1debian11", "mariadb": "11.3.2", "openvino-model-server": "2024.3.0e33a366b", \
-                               "pytorch": "2.2.1", "sklearn": "4144d24ab6852c41a926d279ab673f6259294bccbba2b834b96c681a62f2b865",
+                               "pytorch": "2.2.1", "sklearn": "c0647ca260ebca3acf34b944ce1ea944522382901d1a8a0841af4799366bb326",
                                "memcached": "1.6.14"}
 PYTORCH_UPDATE_APP_VERSION  = f"sed -i 's\\2.0.1-cuda11.7-cudnn8-runtime\\latest\\g' Dockerfile"
 SKLEARN_UPDATE_APP_VERSION  = f"sed -i 's\\scikit-learn-2023.1.1-xgboost-1.7.5-pip-base\\latest\\g' Dockerfile"
